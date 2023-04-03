2df40174dd5ed2ce59d3515b0464b7883e6b9de7
Yashvir <yashvir.singh@tychotechnologies.in>
2023-04-03 18:57:02 +0530

    appId and device token added

--- android/app/src/main/kotlin/com/tycho/tychostreams/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tycho.tychostreams
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/model/razorpay_credentials.dart
+++
@@ -1,5 +0,0 @@
-class RazorpayAuth{
-  // test mode keys
-  static String keyId = 'rzp_test_noV2HU1o1sLUon';
-  static String keySecret = 'RVb60Oo0Ii8RVxMDDWSTZyCe';
-}
